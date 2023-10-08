ce694dffe0c5f8ac5f4f953c4bc430fa40310f7d
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-10-08 17:10:28 +0530

    added notification model

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cron = require('node-cron');
 
 const { PORT } = require('./config/serverConfig');
 const { sendEmail } = require('./services/email-service');
@@ -11,13 +12,19 @@ const startServer = () => {
 
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`)
-        // sendEmail(
-        //     "support@admin.com", 
-        //     "mailing.service.dev.kartik@gmail.com",
-        //     "test email",
-        //     "linking this ?"
-        // );
     })
 }
 
 startServer();
+
+// sendEmail(
+//     "support@admin.com", 
+//     "mailing.service.dev.kartik@gmail.com",
+//     "test email",
+//     "linking this ?"
+// );
+
+// cron for reminder service
+// cron.schedule('*/1 * * * *', () => {
+//     console.log('running a task every minute');
+// });
